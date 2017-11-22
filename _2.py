75afe03e902fd7c36bb2eaa141423dd9d2003471
xinxiwei <lanyingx@126.com>
2017-11-22 15:42:41 +0800

    日期：2017.11.22 修改内容：python编程从入门到实践 第二章总结2

---
+++ _2.py
@@ -0,0 +1,137 @@
+#! /usr/bin/env python
+#coding=utf-8
+
+
+message = "hello python world"
+print (message)
+
+
+message = "hello PYTHON world !!"
+print (message)
+
+
+print (message.title())  #title是首字母大写
+print (message.upper())  #全部转换成大写字母
+print (message.lower())  #全部转换成小写字母
+
+#合并字符串
+first_name = "xin"
+last_name = "xiwei"
+full_name = first_name + " " + last_name
+print (full_name)
+print ("hello" + " " + full_name.title())
+
+#删除空白
+str1 = " python "
+print (str1.rstrip())  #删除末尾空白
+print (str1.lstrip())  #删除开头空白
+print (str1.strip())   #删除开头末尾空白
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
